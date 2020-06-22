5c4f12aab1e79623c3cf449aa9e0c213e2eb0de5
Minakshi Verma <mvedwan@gmail.com>
2020-06-22 19:02:42 -0400

    cra created

---
+++ conway-gameoflife/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Conway-Game of life</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ conway-gameoflife/src/Cell.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Cell = ()=>{
+    
+    return(
+        <div>
+            
+            className="Cell"
+           
+        </div>    
+    )
+}
+ export default Cell
