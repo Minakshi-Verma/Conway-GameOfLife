de23b3b16f29a4706d3d24afdb74779009aba81c
Minakshi Verma <mvedwan@gmail.com>
2020-06-23 00:35:25 -0400

    Board created!

--- conway-gameoflife/src/App.js
+++ conway-gameoflife/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Cellular from './Cellular'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
      <h1>Conway-Game of life</h1>
+     <Cellular />
     </div>
   );
 }
---
+++ conway-gameoflife/src/Cellular.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Cell from './Cell'
+import "./Cellular.css";
+
+const CELL_SIZE = 30;
+const WIDTH = 900;
+const HEIGHT = 600;
+
+class Cellular extends React.Component {
+  constructor() {
+    super()
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+  }
+
+  state = {
+    cells: [] 
+  } 
+  render() {
+    const { cells, interval, isRunning } = this.state;
+    return (
+      <div>      
+        <div
+          className="Twodspace"
+          style={{
+            width: WIDTH,
+            height: HEIGHT,
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
+          }}
+      
+        >
+          {cells.map((cell) => (
+            <Cell x={cell.x} y={cell.y} key={`${cell.x}, ${cell.y}`} />
+          ))}       
+        </div>
+               
+      </div>
+    )
+  }
+}
+
+export default Cellular;
