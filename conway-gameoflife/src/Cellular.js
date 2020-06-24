d9cb1933d0fec6a1ed1d9cfc950142fd4b8d26c6
Minakshi Verma <mvedwan@gmail.com>
2020-06-23 22:32:18 -0400

    Cell state is not toggling between alive and dead.

--- conway-gameoflife/src/Cellular.js
+++ conway-gameoflife/src/Cellular.js
@@ -11,11 +11,67 @@ class Cellular extends React.Component {
     super()
     this.rows = HEIGHT / CELL_SIZE;
     this.cols = WIDTH / CELL_SIZE;
+    this.automaton = this.makeEmptyAutomaton()
   }
 
   state = {
-    cells: [] 
+    cells: [],
+    interval:50,
+    isRunning: false 
   } 
+
+makeEmptyAutomaton(){
+    let automaton =[]
+    for(let y=0;y<this.rows;y++){
+        automaton[y]=[]
+        for (let x=0; x< this.cols;x++){
+            automaton[y][x]= false;
+        }
+    }
+    return automaton
+}
+
+//   Add makeCells function to create newcells
+  makeCells() {
+    let cells = [];
+    for (let y = 0; y < this.cols; y++) {
+      for (let x = 0; x < this.rows; x++) {
+        if (this.automaton[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;
+  }
+
+//getElementOffset function
+
+  getElementOffset() {
+    const rect = this.automatonRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: rect.left + window.pageXOffset - doc.clientLeft,
+      y: rect.top + window.pageYOffset - doc.clientTop,
+    };
+  }
+
+//  Add onchange handler 
+    handleClick = (e) => {
+    const elemOffset = this.getElementOffset()
+    const offsetX = e.clientX - elemOffset.x
+    const offsetY = e.clientY - elemOffset.y
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    if (x >= 0 && x <= this.cols && y >= 0 && y <= this.rows) {
+      this.automaton[y][x] = !this.automaton[y][x]
+    }
+
+    this.setState({ cells: this.makeCells() })
+  }
+
   render() {
     const { cells, interval, isRunning } = this.state;
     return (
@@ -27,6 +83,10 @@ class Cellular extends React.Component {
             height: HEIGHT,
             backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`,
           }}
+          onClick={this.handleClick}
+          ref={(n) => {
+            this.automatonRef = n;
+          }}
       
         >
           {cells.map((cell) => (
@@ -34,28 +94,37 @@ class Cellular extends React.Component {
           ))}       
         </div>
 
-     
+        {/* ------- */}
 
         <div className="controls">
           <div className="updateInput">
-            Update every
+            Update every 
             <input
-            />
-            msec
-          </div>
-          <div className="buttons">        
-            <button className="button">
-              Stop
-            </button>          
-            <button className="button" >
-              Run
-            </button>     
+            value={this.interval}
             
-            <button className="button">
+            />
+            msec           
+          </div>
+          <div className="buttons"> 
+          {isRunning ? (              
+            <button className="button" onClick={this.stopGame}>
+              Stop
+            </button> 
+            ) : (          
+            <button className="button" onClick={this.runGame} >
+              Run
+            </button>
+             
+            )}                     
+         
+            <button className="button" onClick={this.clearGame} >
             Clear
             </button>
-          </div>
+          </div> 
         </div>
+
+
+       
                
       </div>
     )
--- conway-gameoflife/src/Header.js
+++ conway-gameoflife/src/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import About from './About'
 import "./Header.css"
 
 const Header = ()=>{
